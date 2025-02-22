2f7181f791983ad0cc1597980ec5d6325dcdb496
Paolino1988 <49198785+Paolino1988@users.noreply.github.com>
2025-02-22 07:25:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ server = app.server  # Render ha bisogno di questa variabile per Gunicorn
 app.layout = html.Div("Hello, World!")
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=5000)
+    app.run_server(debug=True, host="0.0.0.0", port=5000)
